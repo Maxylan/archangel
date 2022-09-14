54bcec8e013242ee29c47e6a0474cceddada73d3
Max <max@newseed.se>
2022-09-14 17:38:44 +0200

    Removed build-files from the repo.

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const config = {
-    ARCHANGEL_TOKEN: process.env.ARCHANGEL_TOKEN,
-};
-exports.config = config;
